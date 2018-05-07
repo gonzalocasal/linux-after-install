5f98a1f9bdf4fdf9d53b5dea276e7a1febcdfbd2
Gonzalo <gonzalocasal@gmail.com>
2018-05-07 11:39:58 -0300

    Update personal.sh

--- personal.sh
+++ personal.sh
@@ -2,27 +2,8 @@
 #    PERSONAL SETTINGS    #
 ###########################
 
-
-# THEME
-sudo dpkg -i Files/Theme/arc.deb
-sudo apt install -f -y
-sleep 10
-
-# NVIDIA DRIVERS
-sudo apt install -y nvidia-367 nvidia-prime
-sleep 10
-
 #ECLIPSE SETTINGS
 cp -rf Files/Eclipse/eclipse.ini /home/gonzalo/Programas/eclipse/
-
-
-# CLEANUP FLATPAK .DESKTOPS FILES
-sudo rm /var/lib/flatpak/app/org.libreoffice.LibreOffice/current/active/export/share/applications/*
-sudo rm /var/lib/flatpak/app/com.github.bitseater.weather/current/active/export/share/applications/*
-sudo rm /var/lib/flatpak/app/com.github.wwmm.pulseeffects/current/active/export/share/applications/*
-
-# HIDE MEGASYNC IN WINGPANEL
-sudo sh -c "echo 'megasync' >> /etc/wingpanel.d/ayatana.blacklist"
 
 # WINDOWS KEY
 gsettings set org.gnome.mutter overlay-key "'Super_L'"
