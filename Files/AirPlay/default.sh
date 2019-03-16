cef0f1e13910fffa37ea8e007e3ef4800138eced
gonzalo <gonzalocasal@gmail.com>
2019-03-16 08:46:05 -0300

    script to load hotkeys after reboot

--- Files/AirPlay/default.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash 
-sleep 5
-pacmd unload-module module-raop-discover
--- config.sh
+++ config.sh
@@ -24,25 +24,10 @@ echo "###################    USEFUL HOTKEYS  ######################"
 echo "################################################################"
 cp -a Files/AirPlay/toggle.sh ~/.toggle.sh
 chmod +x ~/.toggle.sh
-gsettings set org.gnome.mutter overlay-key "'Super_L'"
-gsettings set org.gnome.settings-daemon.plugins.media-keys play '<Primary><Shift>space'
-gsettings set org.gnome.settings-daemon.plugins.media-keys next '<Primary><Shift>Right'
-gsettings set org.gnome.settings-daemon.plugins.media-keys previous '<Primary><Shift>Left'
-gsettings set org.gnome.settings-daemon.plugins.media-keys volume-down '<Primary><Shift>Down'
-gsettings set org.gnome.settings-daemon.plugins.media-keys volume-up '<Primary><Shift>Up'
-gsettings set org.gnome.desktop.wm.keybindings minimize "['<Super>d']"
-gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/']"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "'gala restart'"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding "'<Ctrl><Alt>g'"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "'killall -HUP gala'"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ name "'xkill'"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ binding "'<Ctrl><Alt>x'"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ command "'xkill'"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/ name "'AirPlay'"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/ binding "'<Ctrl><Shift>A'"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/ command "'/bin/sh /home/gonzalo/.toggle.sh'"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/ binding "'<Super>space'"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/ command "'slingscold'"
+
+cp -a Files/Hotkeys/hotkeys.sh ~/.hotkeys.sh
+chmod +x ~/.hotkeys.sh
+
 
 echo "################################################################"
 echo "###################    ICONS   ######################"
