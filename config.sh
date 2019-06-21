7dff50c1db426f454fa04de93c3b2f9ce97b1a4e
gonzalo <gonzalocasal@gmail.com>
2019-06-21 08:55:35 -0300

    plank

--- config.sh
+++ config.sh
@@ -19,28 +19,19 @@ echo "alias walls='sudo -S <<< "PASS" rm -r /usr/share/wallpapers/*; sudo cp -a 
 source ~/.bashrc
 
 echo "################################################################"
-echo "###################    DLNA ALIAS   ######################"
-echo "################################################################"
-echo "alias dlna='sudo -S <<< "PASS" pulseaudio-dlna --encoder wav'" >> ~/.bashrc
-source ~/.bashrc  
-
-echo "################################################################"
-echo "###################   JAVA FONTS ######################"
-echo "################################################################"
-echo "export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=[setting]'" >> ~/.bashrc
-source ~/.bashrc  
-
-echo "################################################################"
 echo "###################    DESKTOPS FILES   ######################"
 echo "################################################################"
 cp -a Files/Desktops/. ~/.local/share/applications/
 
 echo "################################################################"
-echo "###################    THEMES   ######################"
+echo "###################    THEME   ######################"
 echo "################################################################"
-mkdir ~/.local/share/aurorae
-mkdir ~/.local/share/aurorae/themes
-cp -a Files/Themes/. ~/.local/share/aurorae/themes/
+kwriteconfig5 --file ~/.config/plasmarc --group Theme --key name breeze
+
+echo "################################################################"
+echo "###################    PLANKTHEME   ######################"
+echo "################################################################"
+cp -r Files/Themes/plank-custom ~/.local/share/plank/themes/plank-custom
 
 echo "################################################################"
 echo "###################    FONTS   ######################"
@@ -66,12 +57,3 @@ echo "################################################################"
 echo "###################   CLEMENTINE   ######################"
 echo "################################################################"
 yes | cp Files/Config/Clementine.conf  ~/.config/Clementine/
-
-echo "################################################################"
-echo "###################   LATTE CONFIG   ######################"
-echo "################################################################"
-cp Files/transparent.png  ~/.transparent.png
-cp Files/latte.sh  ~/.config/autostart/
-cp Files/latte.sh.desktop  ~/.config/autostart/
-latte-dock --import-layout Files/plasma_dark.layout
-latte-dock --import-layout Files/plasma_light.layout
